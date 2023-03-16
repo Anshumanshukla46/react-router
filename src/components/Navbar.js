742cca69ca1edbafa74ac085cee5c9102bb98d2a
Anshuman Shukla <Anshumanshukla46@gmail.com>
2023-03-16 22:03:58 +0530

    Link and NavLink

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import { NavLink } from 'react-router-dom'
+// it is similar to anchor tag (check index.js)
+
+function Navbar() {
+
+    // using active
+
+    // get from NavLink
+    const navLinkStyles = ({ isActive }) => {
+        return {
+            textDecoration: isActive ? 'none' : 'underline',
+            fontWeight: isActive ? 'bolder' : "normal"
+        }
+    }
+
+    return (
+        <nav>
+            {/* inspected we can see it is anchor tag */}
+            {/* NavLink shows when some is active or not just by adding className at the time of running code SO STYLE IT USING ACTIVE className */}
+            <NavLink
+                style={navLinkStyles}
+                to='/'>
+                Home
+            </NavLink>
+
+            {/* not similar with route in App.j */}
+            <NavLink
+                style={navLinkStyles}
+                to="/about">
+                About
+            </NavLink>
+        </nav>
+    )
+}
+
+export default Navbar
